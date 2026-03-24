4a1f0da3cbeb3fcbca01f246fe01084de06e02dd
Finnstan <143432729+Finnstan@users.noreply.github.com>
2026-03-24 14:22:08 -0400

    Add files via upload

--- Mastermind.h
+++ Mastermind.h
@@ -1,18 +1,26 @@
-#pragma once
 #ifndef MASTERMIND_H
 #define MASTERMIND_H
 
 #include <iostream>
 #include "Sequence.h"
+#include "Solver.h"
 
 using namespace std;
 class Mastermind
 {
 public:
-	void printColors();
-	void makeSequence();
+	void gameChoice();					//used to see if user wants to use solver or game
+	void gamePlay();					//basic gameplay process
+	void printColors();					//prints the valid colors
+	bool choiceValidation(char c);		//checks to see if the choice in gameChoice is valid
+	bool validateColor(char c);			//checks to see if the char for a color is valid
+	void makeSequence();				//makes a sequence given the colors in user input
+	void printSecretCode();				//prints the sequence that is the correct guess
+	void getPlayerGuess();				//gets the guessed sequence from user
+	bool compareSequences();			//compares
 private:
 	Sequence code;
+	Sequence guess;
 };
 
 #endif
